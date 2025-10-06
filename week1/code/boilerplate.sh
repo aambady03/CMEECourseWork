a323efbb822f460c648c4853c9fa55d1e8e2b684
Anaga Ambady <anaga.ambady25@imperial.ac.uk>
2025-10-06 14:17:59 +0100

    test

---
+++ week1/code/boilerplate.sh
@@ -0,0 +1,10 @@
+#!bin/sh
+#Author: aa6725@ic.ac.uk
+#Scripts: boilerplate.sh
+#Desc: simple boilerplate for shell scripts
+#Arguments: none
+#Date: Oct 9
+
+echo -e "\nThis is a shell script! \n"
+
+#exit
---
+++ week1/code/variables.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+## Illustrates the use of variables 
+
+# Special variables
+
+echo "This script was called with $# parameters"
+echo "The script's name is $0"
+echo "The arguments are $@"
+echo "The first argument is $1"
+echo "The second argument is $2"
+
+# Assigned Variables; Explicit declaration:
+MY_VAR='some string' 
+echo 'the current value of the variable is:' $MY_VAR
+echo
+echo 'Please enter a new string'
+read MY_VAR
+echo
+echo 'the current value of the variable is:' $MY_VAR
+echo
+
+## Assigned Variables; Reading (multiple values) from user input:
+echo 'Enter two numbers separated by space(s)'
+read a b
+echo
+echo 'you entered' $a 'and' $b '; Their sum is:'
+echo $MY_SUM
