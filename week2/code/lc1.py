1406b482f6c7e81485a9e62241bf33d2c308c74e
Anaga Ambady <anaga.ambady25@imperial.ac.uk>
2025-11-29 00:22:39 +0000

    week2 ex1

--- week2/code/lc1.py
+++ week2/code/lc1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """Using Converntional Loops and List comprehensions for 
-bird species'mean body masses.
+bird species' mean body masses.
 
 Author: Anaga Ambady (aa6725@ic.ac.uk)
 Version: 1.0.0
@@ -19,30 +19,18 @@ birds = ( ('Passerculus sandwichensis','Savannah sparrow',18.7),
 # each species in birds, respectively. 
 
 #1A. latin names-comprehensions
-
-birds = [['Passerculus sandwichensis','Savannah sparrow',18.7],['Delichon urbica','House martin',19],['Junco phaeonotus','Yellow-eyed junco',19.5],['Junco hyemalis','Dark-eyed junco',19.6], ['Tachycineata bicolor','Tree swallow',20.2]]
-latin_names = [row[0] for row in birds]
-
-ln = ', '.join(map(str, latin_names))
-     
-print(("The latin names of the species are:"), ln)
-
 #1B. common names
-
-birds = [['Passerculus sandwichensis','Savannah sparrow',18.7],['Delichon urbica','House martin',19],['Junco phaeonotus','Yellow-eyed junco',19.5],['Junco hyemalis','Dark-eyed junco',19.6], ['Tachycineata bicolor','Tree swallow',20.2]]
-common_names = [row[1] for row in birds]
-
-cn = ', '.join(map(str, common_names))
-     
-print(("The common names of the species are:"), cn)
-
 #1C. mean body mass
-
-birds = [['Passerculus sandwichensis','Savannah sparrow',18.7],['Delichon urbica','House martin',19],['Junco phaeonotus','Yellow-eyed junco',19.5],['Junco hyemalis','Dark-eyed junco',19.6], ['Tachycineata bicolor','Tree swallow',20.2]]
+latin_names = [row[0] for row in birds]
+common_names = [row[1] for row in birds]
 mbody_mass = [row[2] for row in birds]
 
+ln = ', '.join(map(str, latin_names))
+cn = ', '.join(map(str, common_names)) 
 mm = ', '.join(map(str, mbody_mass))
-     
+
+print(("The latin names of the species are:"), ln)     
+print(("The common names of the species are:"), cn)
 print(("The mean body masses of the species are:"), mm)
 
 #(2) Now do the same using conventional loops (you can choose to do this 
@@ -50,7 +38,7 @@ print(("The mean body masses of the species are:"), mm)
 
 #2A. latin names-conventional loops 
 
-birds = [['Passerculus sandwichensis','Savannah sparrow',18.7],['Delichon urbica','House martin',19],['Junco phaeonotus','Yellow-eyed junco',19.5],['Junco hyemalis','Dark-eyed junco',19.6], ['Tachycineata bicolor','Tree swallow',20.2]]
+
 latin_names = []
 for row in birds:
     latin_names.append(row[0])
@@ -61,7 +49,7 @@ print(("The latin names of the species are:"), ln)
 
 #2B. common names
 
-birds = [['Passerculus sandwichensis','Savannah sparrow',18.7],['Delichon urbica','House martin',19],['Junco phaeonotus','Yellow-eyed junco',19.5],['Junco hyemalis','Dark-eyed junco',19.6], ['Tachycineata bicolor','Tree swallow',20.2]]
+
 common_names = []
 for row in birds:
     common_names.append(row[1])
@@ -72,7 +60,7 @@ print(("The common names of the species are:"), cn)
 
 #2C. mean body mass
 
-birds = [['Passerculus sandwichensis','Savannah sparrow',18.7],['Delichon urbica','House martin',19],['Junco phaeonotus','Yellow-eyed junco',19.5],['Junco hyemalis','Dark-eyed junco',19.6], ['Tachycineata bicolor','Tree swallow',20.2]]
+
 mbody_mass = []
 for row in birds:
     mbody_mass.append(row[2])
