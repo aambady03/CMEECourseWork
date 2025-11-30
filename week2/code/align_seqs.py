7a7012bfc3bc6dde9c50907957bdd26199f5ed3e
Anaga Ambady <anaga.ambady25@imperial.ac.uk>
2025-11-30 22:29:34 +0000

    week2 ex6

--- week2/code/align_seqs.py
+++ week2/code/align_seqs.py
@@ -9,9 +9,15 @@ Author: Anaga Ambady (aa6725@ic.ac.uk)
 Version: 1.0.0
 Date: Oct 2025
 """
+## import ##
 import csv, os
 
+# define functions
+# calculate alignment score based on matches
 def calculate_score(s1, s2, l1, l2, startpoint):
+    """Calculates the aligment score between the shorter
+sequence (s2) and a segment of the longer sequence (s1)
+starting at 'startpoint'"""
     score = 0
     for i in range(l2):
         if (i + startpoint < l1):
@@ -19,18 +25,22 @@ def calculate_score(s1, s2, l1, l2, startpoint):
                 score += 1
     return score
 
+# defines relative file locations
 def main():
+    """Main function to perform sequence alignment
+    and save results"""
     input_file = "../data/sequences.csv"
     output_file = "../results/alignment_result.txt"
     
-    # Read sequences from CSV file
+    # read sequences from CSV file
     with open(input_file, "r") as f:
         reader = csv.reader(f)
         sequences = [row[0].strip() for row in reader if row]
     
     seq1, seq2 = sequences[0], sequences[1]
     
-    # Determine which is longer
+    # determine longer (s1) and shorter (shorter) s2 sequences
+    # longest seq (s1) treated as reference seq
     l1, l2 = len(seq1), len(seq2)
     if l1 >= l2:
         s1, s2 = seq1, seq2
@@ -41,13 +51,14 @@ def main():
     my_best_align = None
     my_best_score = -1
     
-    # Try all possible alignments
+    # find best alignment
     for i in range(l1):
         z = calculate_score(s1, s2, l1, l2, i)
         if z > my_best_score:
             my_best_align = "." * i + s2
             my_best_score = z
     
+    # write result files
     with open(output_file, "w") as out:
         out.write("Best alignment:\n")
         out.write(my_best_align + "\n")
