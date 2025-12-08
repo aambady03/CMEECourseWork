9964700214ac739f242dfcce746ada372841febb
Anaga Ambady <anaga.ambady25@imperial.ac.uk>
2025-12-08 01:13:53 +0000

    fixed error: changed data file name

--- week3/code/basic_io.R
+++ week3/code/basic_io.R
@@ -4,7 +4,7 @@
 ##Description:  A simple script to illustrate R input-output.  
 ## Run line by line and check inputs outputs to understand what is happening  
 
-MyData <- read.csv("../data/TreeHeight.csv", header = TRUE) # import with headers
+MyData <- read.csv("../data/trees.csv", header = TRUE) # import with headers
 
 write.csv(MyData, "../results/MyData.csv") #write it out as a new file
 
