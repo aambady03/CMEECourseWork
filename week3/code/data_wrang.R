a5535c399d9178c6f328a4705068d854c79058c4
Anaga Ambady <anaga.ambady25@imperial.ac.uk>
2025-12-08 01:18:38 +0000

    fixed error: removing 'fix' & 'melt' to run code smoothly

--- week3/code/data_wrang.R
+++ week3/code/data_wrang.R
@@ -20,8 +20,8 @@ MyMetaData <- read.csv("../data/PoundHillMetaData.csv", header = TRUE, sep = ";"
 head(MyData)
 dim(MyData)
 str(MyData)
-fix(MyData) #you can also do this
-fix(MyMetaData)
+#fix(MyData) #you can also do this
+#fix(MyMetaData)
 
 ############# Transpose ###############
 # To get those species into columns and treatments into rows 
@@ -40,7 +40,7 @@ colnames(TempData) <- MyData[1,] # assign column names from original data
 ############# Convert from wide to long format  ###############
 require(reshape2) # load the reshape2 package
 
-?melt #check out the melt function
+#?melt #check out the melt function
 
 MyWrangledData <- melt(TempData, id=c("Cultivation", "Block", "Plot", "Quadrat"), variable.name = "Species", value.name = "Count")
 
